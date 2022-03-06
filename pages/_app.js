6f13321077e3fd32882550626527d633004e84d6
ahadnur <ahadnur44@gmail.com>
2022-03-06 22:27:54 +0600

    first commit of furniture

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import SEO from '@bradgarropy/next-seo'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  return(
+    <>
+      <SEO title="Furniture Haven" description="The best place to find furnitures" />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
---
+++ pages/data.js
@@ -0,0 +1,56 @@
+module.exports = [
+    {
+        "id": 1,
+        "title": "Before & After: “Patience and Perseverance” Save a Rundown Eichler Home With a Major Mold Problem",
+        "subtitle": "Some things were beyond salvaging, but a renovation by interior designer Diona Polito worked to honor the history of the house.",
+        "content": "Dicta aut earum omnis aliquam itaque. Sequi consectetur molestiae rerum corporis. Nihil officiis facilis aspernatur libero alias aut. Accusamus officia voluptate aliquid cum aspernatur. Ut consequatur distinctio nihil nesciunt totam excepturi corporis. Eveniet facere ex voluptatum sint dicta perferendis.",
+        "image": "/gallery/1.jpg",
+        "author": "Benjamin",
+        "tag": ["latest", "exclusive"]
+    },
+    {
+        "id": 2,
+        "title": "Why you are not having a great home like this in the lowest cost",
+        "subtitle": "Some things were beyond salvaging, but a renovation by interior designer Diona Polito worked to honor the history of the house.",
+        "content": "Dicta aut earum omnis aliquam itaque. Sequi consectetur molestiae rerum corporis. Nihil officiis facilis aspernatur libero alias aut. Accusamus officia voluptate aliquid cum aspernatur. Ut consequatur distinctio nihil nesciunt totam excepturi corporis. Eveniet facere ex voluptatum sint dicta perferendis.",
+        "image": "/gallery/2.jpg",
+        "author": "Nur",
+        "tag": ["latest", "exclusive", "current"]
+    },
+    {
+        "id": 3,
+        "title": "Let's create a home for you",
+        "subtitle": "Some things were beyond salvaging, but a renovation by interior designer Diona Polito worked to honor the history of the house.",
+        "content": "Accusamus officia voluptate aliquid cum aspernatur. Ut consequatur distinctio nihil nesciunt totam excepturi corporis. Eveniet facere ex voluptatum sint dicta perferendis. Dicta aut earum omnis aliquam itaque. Sequi consectetur molestiae rerum corporis. Nihil officiis facilis aspernatur libero alias aut. ",
+        "image": "/gallery/3.jpg",
+        "author": "Thomas",
+        "tag": ["latest", "exclusive"]
+    },
+    {
+        "id": 4,
+        "title": "Would you like have a home for your family?",
+        "subtitle": "Some things were beyond salvaging, but a renovation by interior designer Diona Polito worked to honor the history of the house.",
+        "content": "Eveniet facere ex voluptatum sint dicta perferendis. Accusamus officia voluptate aliquid cum aspernatur. Ut consequatur distinctio nihil nesciunt totam excepturi corporis.  Dicta aut earum omnis aliquam itaque. Sequi consectetur molestiae rerum corporis. Nihil officiis facilis aspernatur libero alias aut. ",
+        "image": "/gallery/4.jpg",
+        "author": "Thomas",
+        "tag": ["latest",]
+    },
+    {
+        "id": 5,
+        "title": "Who does not want a house starting from $100",
+        "subtitle": "Some things were beyond salvaging, but a renovation by interior designer Diona Polito worked to honor the history of the house.",
+        "content": "Eveniet facere ex voluptatum sint dicta perferendis. Accusamus officia voluptate aliquid cum aspernatur. Ut consequatur distinctio nihil nesciunt totam excepturi corporis.  Dicta aut earum omnis aliquam itaque. Sequi consectetur molestiae rerum corporis. Nihil officiis facilis aspernatur libero alias aut. ",
+        "image": "/gallery/5.jpg",
+        "author": "John",
+        "tag": ["exclusive", "current"]
+    },
+    {
+        "id": 6,
+        "title": "Hello, I am a real estate agent",
+        "subtitle": "Some things were beyond salvaging, but a renovation by interior designer Diona Polito worked to honor the history of the house.",
+        "content": "Eveniet facere ex voluptatum sint dicta perferendis. Accusamus officia voluptate aliquid cum aspernatur. Ut consequatur distinctio nihil nesciunt totam excepturi corporis.  Dicta aut earum omnis aliquam itaque. Sequi consectetur molestiae rerum corporis. Nihil officiis facilis aspernatur libero alias aut. ",
+        "image": "/gallery/6.jpg",
+        "author": "John",
+        "tag": ["latest", 'current']
+    },
+];
