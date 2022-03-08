4926b587174366a6551554cafd4d967b5e8ebd3b
ahadnur <ahadnur44@gmail.com>
2022-03-08 18:46:47 +0600

    responsive updated

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ import currentBanner from '../public/currentBanner.jpg'
 
 export default function Home() {
   return (
-    <div className="xl:px-[250px] sm:px-10">
+    <div className="xl:px-[250px] sm:px-10 px-6">
       <header className="flex justify-between items-center py-4 xl:flex-row sm:flex-row flex-col w-full">
         <div className="flex flex-start rounded-sm flex-1 mb-2">
           <Image src={search} width={30} height={30} alt="Search" />
@@ -48,15 +48,15 @@ export default function Home() {
       </nav>
 
       <main>
-        <div className="hero bg-gray-300 h-96 w-full overflow-hidden pointer">
+        <div className="hero bg-gray-300 h-60 sm:h-96 w-full overflow-hidden pointer">
           <Image 
             src={heroImage} 
             alt="Hero Image"
             objectFit='contain'
           />
         </div>
-        <div className="content flex mt-10">
-          <div className="leftSite w-2/3">
+        <div className="content sm:flex mt-10 flex-col sm:flex-row" >
+          <div className="leftSite w-full sm:w-2/3">
             <div className="cardContent">
               <h1 className="text-4xl font-bold mb-2 max-w-4xl hover:opacity-75 cursor-pointer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, et.</h1>
               <p className="">Fuga deleniti laboriosam recusandae ex, ullam illo.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga deleniti laboriosam recus.</p>
@@ -71,7 +71,7 @@ export default function Home() {
             <div className="mt-12">
               {
                 data.map((d, id) => (
-                  <div className="image flex mb-4" key={id}>
+                  <div className="image flex flex-col sm:flex-row mb-4" key={id}>
                     <Image
                       src={d.image}
                       alt="Content Image"
@@ -79,7 +79,7 @@ export default function Home() {
                       width={300}
                       objectFit="cover"
                     />
-                    <div className="text ml-4">
+                    <div className="text sm:ml-4">
                       <h2 className="text-2xl font-bold mb-2 max-w-4xl hover:opacity-75 cursor-pointer">{d.title}</h2>
                       <p>{d.subtitle}</p>
                       <span className="text-xs text-emerald-600">By - {d.author}</span>
@@ -89,7 +89,7 @@ export default function Home() {
               }
             </div>
           </div>
-          <div className="rightSide w-1/3 ml-8">
+          <div className="rightSide w-full sm:w-1/3 sm:ml-8">
             <div className="current">
               <p className="border-b-2 border-gray-900 text-lg font-semibold mb-4">Current Issue</p>
               <div className="w-full">
